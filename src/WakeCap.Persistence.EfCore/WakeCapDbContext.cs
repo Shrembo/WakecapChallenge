bcfa635d0ecf34e053c05cf52028482ea5567e46
shrembo <mshreem00@gmail.com>
2025-03-22 06:32:20 +0300

    Add UTC conversion for DateTime in WakeCapDbContext
    This commit introduces a new using directive for
    `Microsoft.EntityFrameworkCore.Storage.ValueConversion`
    and adds a private static method `SetTimeUtcConverter`
    to the `WakeCapDbContext` class. This method configures
    a `ValueConverter` for `DateTime` properties to ensure
    they are stored in UTC format in PostgreSQL. The converter
    is applied to all `DateTime` properties during model
    creation, addressing the issue of writing `DateTime`
    values with `Kind=Unspecified` to PostgreSQL, which
    only supports UTC timestamps.

--- src/WakeCap.Persistence.EfCore/WakeCapDbContext.cs
+++ src/WakeCap.Persistence.EfCore/WakeCapDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 
 namespace WakeCap.Persistence.EfCore;
@@ -8,6 +9,25 @@ public sealed class WakeCapDbContext(DbContextOptions<WakeCapDbContext> options)
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(WakeCapDbContext).Assembly);
+        SetTimeUtcConverter(modelBuilder);
+
         base.OnModelCreating(modelBuilder);
+    }
+
+
+    // Cannot write DateTime with Kind=Unspecified to PostgreSQL type 'timestamp with time zone', only UTC is supported
+    private static void SetTimeUtcConverter(ModelBuilder modelBuilder)
+    {
+        var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+                    v => v.Kind == DateTimeKind.Unspecified ? DateTime.SpecifyKind(v, DateTimeKind.Utc) : v.ToUniversalTime(),
+                    v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+        {
+            foreach (var property in entityType.GetProperties().Where(p => p.ClrType == typeof(DateTime)))
+            {
+                property.SetValueConverter(dateTimeConverter);
+            }
+        }
     }
 }
